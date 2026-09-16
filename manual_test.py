6e8350f7a6d54406381795c3d64c2e865099bca2
ELM31 <31elopez@gmail.com>
2026-09-16 03:34:22 -0700

    S1-R04-T3: Implement native manual test script

---
+++ manual_test.py
@@ -0,0 +1,8 @@
+import time
+from src.slidekick.presentation_adapter import KeyboardPresentationAdapter
+
+print("Switching slides in 3 seconds. Click into the browser!")
+time.sleep(3)
+    
+adapter = KeyboardPresentationAdapter()
+adapter.next_slide()
